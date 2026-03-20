31ab5e88629a8110aedb411ba64e53394910f25f
jmnsf <jmarcosnsf@gmail.com>
2026-03-19 22:13:06 -0300

    initial commit

---
+++ m.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"UrlShortener/api"
+	"log/slog"
+	"net/http"
+	"time"
+)
+
+func main() {
+	if err := run(); err != nil {
+		slog.Error("failed to execute code", "error", err)
+		return
+	}
+	slog.Info("all systems offilne")
+}
+
+func run() error {
+	handler := api.NewHandler()
+
+	s := http.Server{
+		ReadTimeout: 10 * time.Second,
+		IdleTimeout: time.Minute,
+		WriteTimeout: 10 * time.Second,
+		Addr: ":8080",
+		Handler: handler,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
+}
